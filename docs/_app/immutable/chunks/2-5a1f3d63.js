8d0c4573cc5e360c836becfd6809aba87c623037
edfii <edith.fink@gmx.net>
2024-03-06 19:00:22 +0100

    update headline

---
+++ docs/_app/immutable/chunks/2-5a1f3d63.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-2d617c3c.js";export{t as component};
--- docs/_app/immutable/chunks/2-f807d36c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-cc215028.js";export{t as component};
